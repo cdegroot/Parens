66dfa4f5ae03482cfc87cb7206d899f6e61d5237
Cees de Groot <cg@cdegroot.com>
2012-04-22 02:29:15 +0200

    Function definition and evaluation seem to work

--- src/main/scala/parsing/Tokens.scala
+++ src/main/scala/parsing/Tokens.scala
@@ -2,49 +2,76 @@ package parsing
 
 object Tokens {
   
-	sealed abstract class Token { def eval: Token } 
+	sealed abstract class Token { 
+	  def eval: Token; 
+	  def bind(binding: Binding): Token 
+	} 
 	
 	abstract class BaseSexp extends Token
-	case class Atom(name: String) extends BaseSexp { def eval = this }
-	case class Sexp(car: Token, cdr: Token) extends BaseSexp {def eval = Sexp(car.eval, cdr.eval)}
+	case class Atom(name: String) extends BaseSexp { def eval = this; def bind(binding: Binding) = this }
+	case class Sexp(car: Token, cdr: Token) extends BaseSexp {
+	  def eval = Sexp(car.eval, cdr.eval)
+	  def bind(binding: Binding) = Sexp(car.bind(binding), cdr.bind(binding)) 
+	}
 	
 	val T = Atom("T")
 	val F = Atom("F")
 	val NIL = Atom("NIL")
 	
 	case class Var(name: String) extends Token { 
-	  def eval = this // Later on, we want variable binding here, I think :)
+	  def eval = this
+      def bind(binding: Binding) = if (binding.name.equals(name)) binding.value else this
 	}
 	
-	case class Fun(name: Var, args: List[Var], body: Token) extends Token {
+	case class Binding(name: String, value: Token)
+	case class Fun(name: Var, arguments: List[Var], body: Token) extends Token {
 	  def eval = throw new Exception("eval(Fun) undefined")
+	  
+	  def callWith(bindings: List[Binding]) = {
+	    // check for valid arguments
+	    bindings.map(binding => 
+	    	    arguments.find(arg => arg.name.equals(binding.name)).getOrElse(
+	    	    		throw new Exception("Unknown argument name " + binding.name)
+	    	    ));
+
+	    // a very functional binding - optimization comes later :P
+	    bindings.foldLeft(this: Token)((fun, arg) => fun.bind(arg)).eval
+	  }
+	  def bind(binding: Binding) = {
+	    body.bind(binding)
+	  }
 	}
 	
 	// elementary functions
 	abstract class ElementaryFunction extends Token
 	case class Cons(left: Token, right: Token) extends ElementaryFunction {
 	  def eval = Sexp(left.eval, right.eval)
+	  def bind(binding: Binding) = Cons(left.bind(binding), right.bind(binding))
 	}
 	case class CdrOf(exp: Token) extends ElementaryFunction {
 	  def eval = exp.eval match {
 	    case sexp:Sexp => sexp.cdr
 	    case _ => throw new Exception("cdr only defined on sexps")
 	  }
+	  def bind(binding: Binding) = CdrOf(exp.bind(binding))
 	}
 	case class CarOf(exp: Token) extends ElementaryFunction {
 	  def eval = exp.eval match {
 	    case sexp:Sexp => sexp.car
 	    case _ => throw new Exception("cdr only defined on sexps")
 	  }
+	  def bind(binding: Binding) = CarOf(exp.bind(binding))
 	}
 
 	// elementary predicates
-	case class EqP(left: Atom, right: Atom) extends ElementaryFunction {
+	case class EqP(left: Token, right: Token) extends ElementaryFunction {
 	  def eval = if (left.eval.equals(right.eval)) T else F
+	  def bind(binding: Binding) = EqP(left.bind(binding), right.bind(binding))
 	}
 	
 	case class AtomP(value: Token) extends ElementaryFunction {
 	  def eval = if (value.eval.isInstanceOf[Atom]) T else F
+	  def bind(binding: Binding) = AtomP(value.bind(binding))
 	}
 
 }
--- src/test/scala/parsing/MetaLanguage.scala
+++ src/test/scala/parsing/MetaLanguage.scala
@@ -1,10 +1,24 @@
 package parsing
+import parsing.Tokens._
 
 class MetaLanguage extends FlatSpecForParsers with MetaLanguageParsers {
 
+  //
+  // Section 1.4 : The LISP Meta-language
+  //
+  
   "The meta language parser" should "handle function definitions" in {
     implicit val parserToTest = functionDefinition
     
-    parsing("third[x]=car[cdr[cdr[x]]]") 
+    parsing("third[x]=car[cdr[cdr[x]]]") should equal(Fun(Var("third"), List(Var("x")),
+        CarOf(CdrOf(CdrOf(Var("x"))))))
+  }
+  
+  they should "handle function evaluation" in {
+    implicit val parserToTest = functionDefinition
+    
+    val fun = parsing("third[x]=car[cdr[cdr[x]]]")
+    
+    fun.callWith(List(Binding("x", parsing("(A B C D)")(sexp)))) should equal(Atom("C"))
   }
 }
