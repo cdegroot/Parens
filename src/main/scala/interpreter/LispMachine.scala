11571949dd60ab8cc2e2d50c92d06d38cef8a77d
Cees de Groot <cg@cdegroot.com>
2012-07-28 20:19:58 +0200

    Moved definition of universal lisp machine functions to a separate object

---
+++ src/main/scala/interpreter/LispMachine.scala
@@ -0,0 +1,74 @@
+package interpreter
+
+object LispMachine {
+  // The expressions building up the universal Lisp Function, and some easy
+  // to handle chunks for unit testing :)
+
+  val equal = "equal[x;y]=[" +
+    "atom[x]→[atom[y]→eq[x;y]; T→F];" +
+    "atom[y]→F;" + // BUG in the original paper.
+    "equal[car[x];car[y]]→equal[cdr[x];cdr[y]];" +
+    "T→F]"
+
+  val subst = "subst[x;y;z]=[equal[y;z]→x;atom[z]→z;T→cons[subst[x;y;car[z]];subst[x;y;cdr[z]]]]"
+
+  // null is not given, but this should work...
+  val nul = "null[x]=[equal[x;NIL]→T;T→F]"
+
+  val append = "append[x;y]=[null[x]→y;T→cons[car[x];append[cdr[x];y]]]"
+  val exprsForAppend = Seq(equal, subst, nul, append)
+
+  val member = "member[x;y]=[" +
+    "null[y] → F;" +
+    "equal[x;car[y]] → T;" +
+    "T → member[x;cdr[y]]]"
+  val exprsForMember = exprsForAppend ++ Seq(member)
+
+  val pairlis = "pairlis[x;y;a] = [" +
+    "null[x]→a; " +
+    "T→cons[cons[car[x]; car[y]]; " +
+    "pairlis[cdr[x]; cdr[y]; a]]]"
+  val exprsForPairlis = exprsForMember ++ Seq(pairlis)
+
+  val assoc = "assoc[x;a]=[" +
+    "equal[caar[a];x]→car[a];" +
+    "T→assoc[x;cdr[a]]]"
+  val exprsForAssoc = exprsForPairlis ++ Seq(assoc)
+
+  val sub2 = "sub2[a;z] = [" +
+    "null[a]→z;" +
+    "eq[caar[a];z]→cdar[a];" +
+    "T→sub2[cdr[a];z]]"
+  val sublis = "sublis[a;y] = " +
+    "[atom[y]→sub2[a;y];" +
+    "T→cons[sublis[a;car[y]];sublis[a;cdr[y]]]]"
+  val exprsForSublis = exprsForAssoc ++ Seq(sub2, sublis)
+
+  // Finishing off with the main juice
+  val evalquote = "evalquote[fn;x] = apply[fn;x;NIL]"
+  val apply = "apply[fn;x;a]=[" +
+    "atom[fn] → [eq[fn;CAR] → caar[x];" +
+    "            eq[fn;CDR] → cdr[x];" +
+    "            eq[fn;CONS]→ cons[car[x];cadr[x]];" +
+    "            eq[fn;ATOM]→ atom[car[x]];" +
+    "            eq[fn;EQ]  → eq[car[x];cadr[x]];" +
+    "            T          → apply[eval[fn;a];x;a]];" +
+    "eq[car[fn];LAMBDA] → eval[caddr[fn];pairlis[cadr[fn];x;a]];" +
+    "eq[car[fn];LABEL]  → aply[caddr[fn];x;cons[cons[cadr[fn];caddr[fn]];a]]]"
+  val eval = "eval[e;a] = [" +
+    "atom[e]      → cdr[assoc[e;a]];" +
+    "atom[car[e]] → [" +
+    "       eq[car[e];QUOTE] → cadr[e];" +
+    "       eq[car[e];COND]  → evcon[cdr[e];a];" +
+    "       T                → apply[car[e];evlis[cdr[e];a];a]];" +
+    "T            → apply[car[e];evlis[cdr[e];a];a]]"
+  val evcon = "evcon[c;a] = [" +
+    "eval[caar[c];a] → eval[cadar[c];a];" +
+    "T               → evcon[cdr[c];a]]"
+  val evlis = "evlis[m;a] = [" +
+    "null[m] → NIL;" +
+    "T       → cons[eval[car[m];a];evlis[cdr[m];a]]]"
+
+  val exprsForEvalquote = exprsForSublis ++ Seq(evalquote, apply, eval, evcon, evlis)
+
+}
--- src/test/scala/parsing/UniversalLispFunction.scala
+++ src/test/scala/parsing/UniversalLispFunction.scala
@@ -1,6 +1,7 @@
 package parsing
 
 import parsing.Tokens.{Token, Atom, Context, EmptyContext}
+import interpreter.LispMachine
 
 class UniversalLispFunction extends FlatSpecForParsers with MetaLanguageParsers {
   implicit val parserToTest = funOrSexp
@@ -17,14 +18,8 @@ class UniversalLispFunction extends FlatSpecForParsers with MetaLanguageParsers 
 
   def evaluating(expr: String)(implicit context: Context): Token = parsing(expr)(parserToTest).eval(context)
 
-  val equal = "equal[x;y]=[" +
-    "atom[x]→[atom[y]→eq[x;y]; T→F];" +
-    "atom[y]→F;" + // BUG in the original paper.
-    "equal[car[x];car[y]]→equal[cdr[x];cdr[y]];" +
-    "T→F]"
-
   "The meta language parsers" should "parse the definition of equal" in {
-    val ctx = buildContextFrom(Seq(equal))
+    val ctx = buildContextFrom(Seq(LispMachine.equal))
     val code = parsing("equal[x;y]")
     code.eval(ctx ++ List(
       "x" -> parsing("(A B)")(sexp),
@@ -34,102 +29,44 @@ class UniversalLispFunction extends FlatSpecForParsers with MetaLanguageParsers 
   val subst = "subst[x;y;z]=[equal[y;z]→x;atom[z]→z;T→cons[subst[x;y;car[z]];subst[x;y;cdr[z]]]]"
 
   they should "parse the definition of subst" in {
-    implicit val ctx = buildContextFrom(Seq(equal, subst))
+    implicit val ctx = buildContextFrom(Seq(LispMachine.equal, subst))
     evaluating("subst[(X . A);B;((A . B) . C)]") should equal(parsing("((A . (X . A)) . C)")(sexp))
   }
 
-  // null is not given, but this should work...
-  val nul = "null[x]=[equal[x;NIL]→T;T→F]"
-
   they should "parse the definition of null" in {
-    implicit val ctx = buildContextFrom(Seq(equal, subst, nul))
+    implicit val ctx = buildContextFrom(Seq(LispMachine.equal, subst, LispMachine.nul))
     evaluating("null[(A . B)]") should equal(Tokens.F)
     evaluating("null[NIL]") should equal(Tokens.T)
   }
 
-  val append = "append[x;y]=[null[x]→y;T→cons[car[x];append[cdr[x];y]]]"
-  val exprsForAppend = Seq(equal, subst, nul, append)
-
   they should "parse the definition of append" in {
-    implicit val ctx = buildContextFrom(exprsForAppend)
+    implicit val ctx = buildContextFrom(LispMachine.exprsForAppend)
     evaluating("append[(A B);(C D E)]") should equal(parsing("(A B C D E)")(sexp))
   }
 
-  val member = "member[x;y]=[" +
-    "null[y] → F;" +
-    "equal[x;car[y]] → T;" +
-    "T → member[x;cdr[y]]]"
-  val exprsForMember = exprsForAppend ++ Seq(member)
-
   they should "parse the definition of member" in {
-    implicit val ctx = buildContextFrom(exprsForMember)
+    implicit val ctx = buildContextFrom(LispMachine.exprsForMember)
     evaluating("member[(A B);(C D (A B) E)]") should equal(Tokens.T)
   }
 
-  val pairlis = "pairlis[x;y;a] = [" +
-    "null[x]→a; " +
-    "T→cons[cons[car[x]; car[y]]; " +
-    "pairlis[cdr[x]; cdr[y]; a]]]"
-  val exprsForPairlis = exprsForMember ++ Seq(pairlis)
-
   they should "parse the definition of pairlis" in {
-    implicit val ctx = buildContextFrom(exprsForPairlis)
+    implicit val ctx = buildContextFrom(LispMachine.exprsForPairlis)
     evaluating("pairlis[(A B C);(U V W);((D . X) (E . Y))]") should equal(parsing("((A . U) (B . V) (C . W) (D . X) (E . Y))")(sexp))
   }
 
-  val assoc = "assoc[x;a]=[" +
-    "equal[caar[a];x]→car[a];" +
-    "T→assoc[x;cdr[a]]]"
-  val exprsForAssoc = exprsForPairlis ++ Seq(assoc)
-
   they should "parse the definition of assoc" in {
-    implicit val ctx = buildContextFrom(exprsForAssoc)
+    implicit val ctx = buildContextFrom(LispMachine.exprsForAssoc)
     evaluating("assoc[B;((A.(M N)), (B .(CAR X)), (C .(QUOTE M)), (C .(CDR X)))]") should equal(parsing("(B . (CAR X))")(sexp))
   }
 
-  val sub2 = "sub2[a;z] = [" +
-    "null[a]→z;" +
-    "eq[caar[a];z]→cdar[a];" +
-    "T→sub2[cdr[a];z]]"
-  val sublis =  "sublis[a;y] = " +
-    "[atom[y]→sub2[a;y];" +
-    "T→cons[sublis[a;car[y]];sublis[a;cdr[y]]]]"
-  val exprsForSublis = exprsForAssoc ++ Seq(sub2, sublis)
-
   they should "parse the definition of sublis" in {
-    implicit val ctx = buildContextFrom(exprsForSublis)
+    implicit val ctx = buildContextFrom(LispMachine.exprsForSublis)
     evaluating("sublis[((X . SHAKESPEARE) (Y . (THE TEMPEST)));(X WROTE Y)]") should  equal(parsing("(SHAKESPEARE WROTE (THE TEMPEST))"))
   }
 
-  // Finishing off with the main juice
-  val evalquote = "evalquote[fn;x] = apply[fn;x;NIL]"
-  val apply = "apply[fn;x;a]=[" +
-    "atom[fn] → [eq[fn;CAR] → caar[x];" +
-    "            eq[fn;CDR] → cdr[x];" +
-    "            eq[fn;CONS]→ cons[car[x];cadr[x]];" +
-    "            eq[fn;ATOM]→ atom[car[x]];" +
-    "            eq[fn;EQ]  → eq[car[x];cadr[x]];" +
-    "            T          → apply[eval[fn;a];x;a]];" +
-    "eq[car[fn];LAMBDA] → eval[caddr[fn];pairlis[cadr[fn];x;a]];" +
-    "eq[car[fn];LABEL]  → aply[caddr[fn];x;cons[cons[cadr[fn];caddr[fn]];a]]]"
-  val eval = "eval[e;a] = [" +
-    "atom[e]      → cdr[assoc[e;a]];" +
-    "atom[car[e]] → [" +
-    "       eq[car[e];QUOTE] → cadr[e];" +
-    "       eq[car[e];COND]  → evcon[cdr[e];a];" +
-    "       T                → apply[car[e];evlis[cdr[e];a];a]];" +
-    "T            → apply[car[e];evlis[cdr[e];a];a]]"
-  val evcon = "evcon[c;a] = [" +
-    "eval[caar[c];a] → eval[cadar[c];a];" +
-    "T               → evcon[cdr[c];a]]"
-  val evlis = "evlis[m;a] = [" +
-    "null[m] → NIL;" +
-    "T       → cons[eval[car[m];a];evlis[cdr[m];a]]]"
-
-  val exprsForEvalquote = exprsForSublis ++ Seq(evalquote, apply, eval, evcon, evlis)
 
   they should "parse evalquote" in {
-    implicit val ctx = buildContextFrom(exprsForEvalquote)
+    implicit val ctx = buildContextFrom(LispMachine.exprsForEvalquote)
 
     evaluating("evalquote[(LAMBDA (X Y) (CONS (CAR X) Y)); ((A B) (C D))]") should equal(parsing("(A C D)")(sexp))
   }
